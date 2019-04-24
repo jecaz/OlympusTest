d590d1787b5623b3246bfb83e648c5ac4b0ec665
Jelena <jelena.zobenica1@gmail.com>
2019-04-24 21:19:09 +0200

    Refactor html structure on page-layout-signup and register page and add part of login page

--- olympus/src/app/components/page-layout-signup/login/login.component.ts
+++ olympus/src/app/components/page-layout-signup/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.user = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
   }
-
 }
--- olympus/src/app/components/page-layout-signup/login/login.module.ts
+++ olympus/src/app/components/page-layout-signup/login/login.module.ts
@@ -2,12 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {LoginComponent} from './login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     RouterModule
   ],
